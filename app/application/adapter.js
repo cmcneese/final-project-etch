652d4042f3f25e59c348181fc7d5187751b0847a
Chris McNeese <christophermcneese@mac.com>
2016-12-02 15:49:22 -0600

    Allow User Login

---
+++ app/application/adapter.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+import config from '../config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+
+
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:jwt',
+
+  host: config.DS.host,
+  namespace: config.DS.namespace,
+
+});
--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,12 @@
 
 module.exports = function(environment) {
   var ENV = {
+    DS: {
+      host: 'http://localhost:3333',
+      namespace: 'api',
+    },
+
+
     modulePrefix: 'final-project-etch',
     environment: environment,
     rootURL: '/',
@@ -20,7 +26,14 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    'ember-simple-auth': {
+      authorizer: 'authorizer:token',
+    },
+
+    'ember-simple-auth-token': {},
+
   };
 
   if (environment === 'development') {
@@ -45,6 +58,8 @@ module.exports = function(environment) {
   if (environment === 'production') {
 
   }
+
+  ENV['ember-simple-auth-token'].serverTokenEndpoint = `${ENV.DS.host}/api/token-auth`;
 
   return ENV;
 };
