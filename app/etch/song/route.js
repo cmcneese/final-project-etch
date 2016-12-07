727940a54c1f48d082eab378aa7287a075c0315a
Christina Keelan <christinakeelan@gmail.com>
2016-12-07 15:31:27 -0600

    create song search modal

---
+++ app/etch/song/route.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  // var templateSource = document.getElementById('results-template').innerHTML,
+  //   template = Handlebars.compile(templateSource),
+  //   resultsPlaceholder = document.getElementById('results'),
+  //   playingCssClass = 'playing',
+  //   audioObject = null;
+  //
+  // var fetchTracks = function (albumId, callback) {
+  //     $.ajax({
+  //         url: 'https://api.spotify.com/v1/albums/' + albumId,
+  //         success: function (response) {
+  //             callback(response);
+  //         }
+  //     });
+  // };
+  //
+  // var searchAlbums = function (query) {
+  //     $.ajax({
+  //         url: 'https://api.spotify.com/v1/search',
+  //         data: {
+  //             q: query,
+  //             type: 'album'
+  //         },
+});
