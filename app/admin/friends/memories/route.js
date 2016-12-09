02b003ad8eb7f7de601359035b2112f5b696bfb3
Christina Keelan <christinakeelan@gmail.com>
2016-12-08 20:54:14 -0600

    scrolling memories timeline with ember data

--- app/admin/friends/memories/route.js
+++ app/admin/friends/memories/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  store: Ember.inject.service(),
+  spotify: Ember.inject.service('spotify-api'),
+
+  model(params) {
+    // TODO: use spotify service to fetch tracks for each memory
+    return this.store.query('memory', { user: params.user_id });
+  }
 });
--- app/services/spotify-api.js
+++ app/services/spotify-api.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 const SPOTIFY_URL = 'https://api.spotify.com/v1/search';
 
 export default Ember.Service.extend({
+
+  get(id) {
+    // TODO put function here for single track by id
+
+  },
+
   tracks(query) {
     const url = `${SPOTIFY_URL}?q=${query}&type=track`;
     return fetch(url)
